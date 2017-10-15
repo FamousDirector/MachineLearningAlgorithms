695ac5151259890e136b17d41aa06a6048b43041
TheFamousDirector <FamousDirector@users.noreply.github.com>
2017-10-15 14:12:15 -0300

    created class to perform kfold cross validation

---
+++ CrossValidation.java
@@ -0,0 +1,12 @@
+public class CrossValidation {
+
+    public static void main(String[] args){
+
+    }
+
+    public static long kFold(int k, Classifier classifier){
+        long averageAccuracy = 0;
+        return averageAccuracy;
+    }
+
+}
