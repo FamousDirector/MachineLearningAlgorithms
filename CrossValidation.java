513ed7df27fa2b5d64d4fdd019d6e6ea01a1f998
TheFamousDirector <FamousDirector@users.noreply.github.com>
2017-10-19 09:18:06 -0300

    added kfold validation method

--- CrossValidation.java
+++ CrossValidation.java
@@ -16,7 +16,36 @@ public class CrossValidation {
         this.variance = variance;
     }
 
+    public static CrossValidation kFold(int k, Classifier classifier, ClassifierData classifierData) {
+        double[] errors = new double[k];
+        int numberOfDataPointsPerK = classifierData.getDataArray().length / k;
 
+        for (int i = 0; i < k; i++) {
+            int lowerBound = k * numberOfDataPointsPerK;
+            int upperBound = (k + 1) * numberOfDataPointsPerK;
+
+            ClassifierData lowerTrainingSet = ClassifierData.createSubsetOfClassifierData(classifierData, 0, lowerBound);
+            ClassifierData upperTrainingSet = ClassifierData.createSubsetOfClassifierData(classifierData, upperBound, classifierData.getDataArray().length);
+            ClassifierData fullTrainingSet = ClassifierData.concatenateClassifierData(lowerTrainingSet, upperTrainingSet);
+
+            ClassifierData testSet = ClassifierData.createSubsetOfClassifierData(classifierData, lowerBound, upperBound);
+            String testSetData[][] = testSet.getDataArray();
+            String testSetClass[] = testSet.getClassArray();
+
+            classifier.reTrain(fullTrainingSet);
+
+            int incorrectClassCount = 0;
+            for (int j = 0; j < testSet.getDataArray().length; j++) {
+                String result = classifier.classify(testSetData[j]);
+                if (result != testSetClass[j]) {
+                    ++incorrectClassCount;
+                }
+            }
+            errors[k] = incorrectClassCount / testSet.getDataArray().length;
+        }
+
+
+        return new CrossValidation(errors);
     }
 
     public static long kFold(int k, Classifier classifier){
