bc15333ad66001c4188b42812bd1ab4aa357b225
TheFamousDirector <FamousDirector@users.noreply.github.com>
2017-12-06 17:01:27 -0400

    fleshed out NB
    still need to fully test

--- NBClassifier.java
+++ NBClassifier.java
@@ -1,62 +1,141 @@
 import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
 
 public class NBClassifier implements Classifier {
 
     public static void main(String[] args) {
-        String samplePath = "C://Users//james//Code//CS6735//MachineLearningAlgorithms//data//letter-recognition.data";
+        String samplePath = "C://Users//james//Code//CS6735//MachineLearningAlgorithms//data//breast-cancer-wisconsin.data";
         try {
-            ClassifierData fullDataset = new ClassifierData(samplePath, 0);
-            ClassifierData partialDataset = ClassifierData.createSubsetOfClassifierData(fullDataset,0,100);
+            ClassifierData fullDataset = new ClassifierData(samplePath, 6);
+            ClassifierData partialDataset = ClassifierData.createSubsetOfClassifierData(fullDataset,0,10000);
+            partialDataset.removeDataColumn(0);
             NBClassifier nb = new NBClassifier(partialDataset);
-            CrossValidation cv = CrossValidation.kFold(3, nb, partialDataset,3);
-            System.out.println();
+            CrossValidation cv = CrossValidation.kFold(5, nb, partialDataset,5);
+            System.out.println(cv.mean);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     ClassifierData data;
+    private Boolean isDiscrete = true;
+    private HashMap<String,Double> priorMap = new HashMap<>();
+    private HashMap<String,HashMap<Integer,Probabilities>> continuousProbabilityMap = new HashMap<>();
+    private HashMap<String,HashMap<Integer,HashMap<String,Double>>> discreteProbabilityMap = new HashMap<>();
+
     public NBClassifier(ClassifierData classifierData)
     {
         reTrain(classifierData);
     }
 
+    public NBClassifier(ClassifierData classifierData, Boolean isDiscrete)
+    {
+        reTrain(classifierData,isDiscrete);
+    }
+
     public void reTrain(ClassifierData classifierData) {
-        this.data = classifierData;
-        //get data for each unique class
-        Map<String, int[]> classMap = new HashMap<>();
-        for (int i = 0; i < classifierData.getClassArray().length; i++) {
+        if (classifierData.isDataNumeric())
+            reTrain(classifierData, false);
+        else
+            reTrain(classifierData, true);
 
-            String classLabel = classifierData.getClassArray()[i];
-            if(classMap.containsKey(classLabel))
-            {
-                int[] temp = classMap.get(classLabel);
-                classMap.remove(classLabel);
-                int[] newIndex = new int[temp.length+1];
-                newIndex[0] = i;
-                for (int j = 0; j < temp.length; j++) {
-                    newIndex[j] = temp[j];
-                }
-                classMap.put(classLabel,newIndex);
-            }
-            else {
-                classMap.put(classLabel,new int[]{i});
-            }
-        }
-
-        //calculate probabilities for each class
-        for (String classL : classMap.keySet()){
-            int[] index = classMap.get(classL);
-        }
-        //create some sort of map of prob
-
-        //maybe do something different for discreet?
 
     }
 
+    public void reTrain(ClassifierData classifierData, Boolean isDiscrete) {
+        this.data = classifierData;
+        this.isDiscrete = isDiscrete;
+        if (!this.isDiscrete && !data.isDataNumeric()){
+            this.isDiscrete = true;
+            System.out.println("Incorrect assumption, data is discrete");
+        }
+
+        //get data for each unique class
+        for (String label : data.listOfClasses){
+            int count = 0;
+            for (int i = 0; i < data.classArray.length; i++) {
+                String c = data.classArray[i];
+                if(label.equals(c))
+                    count++;
+            }
+            double priorProb = ((double) count)/data.classArray.length;
+            priorMap.put(label,priorProb);
+
+            if(this.isDiscrete)
+            {
+                HashMap<Integer,HashMap<String,Double>> columnAttributeMap = new HashMap<>();
+                for (int i = 0; i < data.getNumberOfDataColumns(); i++) {
+                    String[] col = data.flippedDataArray[i];
+                    HashSet<String> uniqueAttributes = getUniqueAttributes(col);
+                    HashMap<String,Double> attributeMap = new HashMap<>();
+
+                    for (String attr : uniqueAttributes){
+                        count = 0;
+                        for (int j = 0; j < col.length; j++) {
+                            if (attr.equals(col[j]))
+                            {
+                                count++;
+                            }
+                        }
+                        attributeMap.put(attr, ((double) count)/col.length);
+                    }
+                    columnAttributeMap.put(i,attributeMap);
+                }
+                this.discreteProbabilityMap.put(label,columnAttributeMap);
+            }
+            else //continuous
+            {
+                HashMap<Integer,Probabilities> probabilityMap = new HashMap<>();
+                for (int i = 0; i < data.getNumberOfDataColumns(); i++) {
+                    double[] col = ClassifierData.makeDataNumeric(data.flippedDataArray[i]);
+                    probabilityMap.put(i,new Probabilities(col));
+                }
+                continuousProbabilityMap.put(label,probabilityMap);
+            }
+        }
+    }
+
     public String classify(String[] featureArray) {
-        return null;
+        double highestProb = -1;
+        String likelyClass = "";
+        for (String label : data.listOfClasses)
+        {
+            double newProb = priorMap.get(label);
+            for (int i = 0; i < featureArray.length; i++) {
+
+                if(isDiscrete)
+                {
+                    String attr = featureArray[i];
+                    if(discreteProbabilityMap.get(label).get(i).containsValue(attr)) {
+                        double prob = discreteProbabilityMap.get(label).get(i).get(attr);
+                        newProb = newProb * prob;
+                    }
+                    else //attribute never seen with that class
+                    {
+                        newProb = 0;
+                        break;
+                    }
+                }else {
+                    double val = Double.parseDouble(featureArray[i]);
+                    double prob = continuousProbabilityMap.get(label).get(i).pdf(val);
+                    newProb = newProb * prob;
+                }
+            }
+            if(highestProb<newProb)
+                likelyClass = label;
+                highestProb = newProb;
+        }
+        return likelyClass;
+    }
+
+    private HashSet<String> getUniqueAttributes(String[] array)
+    {
+        HashSet<String> set = new HashSet<>();
+        for (int i = 0; i < array.length; i++) {
+            set.add(array[i]);
+        }
+
+        return set;
     }
 
     private class Probabilities {
@@ -97,6 +176,11 @@ public class NBClassifier implements Classifier {
             return Math.sqrt(getVariance(data));
         }
 
+        public double pdf(double x) {
+            double exponent = Math.exp(-(Math.pow(x-mean,2)/(2*Math.pow(stdDev,2))));
+            double val = (1 / (Math.sqrt(2*Math.PI) * stdDev)) * exponent;
+            return val;
+        }
     }
 }
 
