05a29642f9c6d6df77f66f84bbf579da8843df9e
TheFamousDirector <FamousDirector@users.noreply.github.com>
2017-12-07 14:49:03 -0400

    added values for Adaboost class

--- AdaBoost.java
+++ AdaBoost.java
@@ -1,6 +1,13 @@
+import java.util.Dictionary;
+import java.util.HashMap;
+
 public class AdaBoost implements Classifier {
     public Classifier weakClassifier;
     public ClassifierData data;
+    HashMap<Integer,Double> rowWeights = new HashMap<>();
+    HashMap<Integer,Double> modelWeights = new HashMap<>();
+    HashMap <Integer,Classifier> weakClassifiers = new HashMap<>();
+
 
     public static void main(String[] args) {
         String samplePath = "C://Users//james//Code//CS6735//MachineLearningAlgorithms//data//car.data";
@@ -8,8 +15,8 @@ public class AdaBoost implements Classifier {
             ClassifierData fullDataset = new ClassifierData(samplePath, 6);
             ClassifierData partialDataset = ClassifierData.createSubsetOfClassifierData(fullDataset, 0, 10);
 //            partialDataset.removeDataColumn(0);
-            IDThreeClassifier id3 = new IDThreeClassifier(partialDataset,true);
-            CrossValidation cv = CrossValidation.kFold(5, id3, fullDataset, 10);
+            AdaBoost ada = new AdaBoost(partialDataset,new IDThreeClassifier(partialDataset,true));
+            CrossValidation cv = CrossValidation.kFold(5, ada, fullDataset, 10);
             System.out.println(cv.mean);
         } catch (Exception e) {
             e.printStackTrace();
@@ -22,6 +29,9 @@ public class AdaBoost implements Classifier {
     {
         this.data = data;
         this.weakClassifier = classifier;
+        for (int i = 0; i < data.getNumberOfDataRows(); i++) {
+            rowWeights.put(i,1.0/data.getNumberOfDataRows());
+        }
         reTrain(data);
     }
     public void reTrain(ClassifierData classifierData) {
