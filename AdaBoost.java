acb5064c068705ee603905d4e68e489b0b102e2d
TheFamousDirector <FamousDirector@users.noreply.github.com>
2017-12-07 12:48:21 -0400

    added main

--- AdaBoost.java
+++ AdaBoost.java
@@ -2,6 +2,21 @@ public class AdaBoost implements Classifier {
     public Classifier weakClassifier;
     public ClassifierData data;
 
+    public static void main(String[] args) {
+        String samplePath = "C://Users//james//Code//CS6735//MachineLearningAlgorithms//data//car.data";
+        try {
+            ClassifierData fullDataset = new ClassifierData(samplePath, 6);
+            ClassifierData partialDataset = ClassifierData.createSubsetOfClassifierData(fullDataset, 0, 10);
+//            partialDataset.removeDataColumn(0);
+            IDThreeClassifier id3 = new IDThreeClassifier(partialDataset,true);
+            CrossValidation cv = CrossValidation.kFold(5, id3, fullDataset, 10);
+            System.out.println(cv.mean);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
 
     public AdaBoost(ClassifierData data, Classifier classifier)
     {
@@ -17,4 +32,5 @@ public class AdaBoost implements Classifier {
     public String classify(String[] featureArray) {
         return null;
     }
+
 }
