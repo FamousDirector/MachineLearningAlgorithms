f9793a8cd15581a2c64b571b333877ac3b5db905
TheFamousDirector <FamousDirector@users.noreply.github.com>
2017-12-09 21:51:50 -0400

    added ID3 with Adaboost weights

--- AdaBoost.java
+++ AdaBoost.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
+import java.util.*;
 
 public class AdaBoost implements Classifier {
 
@@ -15,25 +12,25 @@ public class AdaBoost implements Classifier {
 //        String samplePath = "C://Users//james//Code//CS6735//MachineLearningAlgorithms//data//mushroom.data"; //0
 //        String samplePath = "C://Users//james//Code//CS6735//MachineLearningAlgorithms//data//test.data";
 
-        try {
-            ClassifierData fullDataset = new ClassifierData(samplePath, 6);
-//            fullDataset.removeDataColumn(0);
-            AdaBoost ada = new AdaBoost(fullDataset, new NBClassifier(fullDataset, true),100);
-            CrossValidation cv = CrossValidation.kFold(5, ada, fullDataset, 1);
-            System.out.println("Error = " + cv.mean);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
 //        try {
-//            ClassifierData fullDataset = new ClassifierData(samplePath, 0);
+//            ClassifierData fullDataset = new ClassifierData(samplePath, 6);
 ////            fullDataset.removeDataColumn(0);
-//            AdaBoost ada = new AdaBoost(fullDataset, new IDThreeClassifier(fullDataset,false));
-//            CrossValidation cv = CrossValidation.kFold(5, ada, fullDataset, 2);
+//            AdaBoost ada = new AdaBoost(fullDataset, new NBClassifier(fullDataset, true),100);
+//            CrossValidation cv = CrossValidation.kFold(5, ada, fullDataset, 1);
 //            System.out.println("Error = " + cv.mean);
 //        } catch (Exception e) {
 //            e.printStackTrace();
 //        }
+
+        try {
+            ClassifierData fullDataset = new ClassifierData(samplePath, 0);
+//            fullDataset.removeDataColumn(0);
+            AdaBoost ada = new AdaBoost(fullDataset, new IDThreeClassifier(fullDataset,false));
+            CrossValidation cv = CrossValidation.kFold(5, ada, fullDataset, 2);
+            System.out.println("Error = " + cv.mean);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public Classifier weakClassifier;
@@ -95,6 +92,9 @@ public class AdaBoost implements Classifier {
             //prepare classifier
             Classifier classifier = weakClassifier.clone(newData);
 
+            //add weak classifier to ensemble
+            weakClassifiers.add(classifier);
+
             //test classifier
             double incorrectSum = 0;
             double totalSum = 0;
@@ -108,7 +108,7 @@ public class AdaBoost implements Classifier {
             }
             double errorRate = incorrectSum / totalSum;
 
-//            System.out.println("Debug = " + errorRate + " Depth: " + numberOfIterations); //debug
+            System.out.println("Debug = " + errorRate + " Depth: " + numberOfIterations); //debug
 
             // set model weights
             double newModelWeight = Math.log((1 - errorRate) / errorRate) + Math.log(data.listOfClasses.size() - 1);
@@ -119,16 +119,13 @@ public class AdaBoost implements Classifier {
                 String[] featureArray = data.getDataArray()[i];
                 if (!classifier.classify(featureArray).equals(data.getClassArray()[i])) //incorrect classification
                 {
-                    double newRowWeight = rowWeights.get(i) * Math.exp(newModelWeight);
+                    double newRowWeight = Math.exp(newModelWeight) * rowWeights.get(i);
                     rowWeights.set(i,newRowWeight);
                 }
             }
 
-            //add weak classifier to ensemble
-            weakClassifiers.add(classifier);
-
             //normalize weights
-            normalizeRowWeights();
+            //normalizeRowWeights();
 
             numberOfIterations++; //next
         }
@@ -172,23 +169,23 @@ public class AdaBoost implements Classifier {
         }
     }
 
-    private static double[] normalizeArray(Double[] distances) {
-        double[] normalizedArray = new double[distances.length];
+    private static double[] normalizeArray(Double[] inputArray) {
+        double[] normalizedArray = new double[inputArray.length];
         double max = 0;
         double min = Double.POSITIVE_INFINITY;
 
         //find min and max
-        for (int i = 0; i < distances.length; i++) {
-            if (distances[i] > max) {
-                max = distances[i];
+        for (int i = 0; i < inputArray.length; i++) {
+            if (inputArray[i] > max) {
+                max = inputArray[i];
             }
-            if (distances[i] < min) {
-                min = distances[i];
+            if (inputArray[i] < min) {
+                min = inputArray[i];
             }
         }
 
-        for (int i = 0; i < distances.length; i++) {
-            normalizedArray[i] = (distances[i] - min) / (max - min);
+        for (int i = 0; i < inputArray.length; i++) {
+            normalizedArray[i] = (inputArray[i] - min) / (max - min);
         }
         return normalizedArray;
     }
@@ -352,5 +349,301 @@ public class AdaBoost implements Classifier {
             }
         }
     }
+
+    private static class IDThreeClassifier implements Classifier {
+
+        private ClassifierData data;
+        private DecisionNode tree;
+        private int maxDepth = -1;
+        private boolean stopEarly = false;
+
+        public IDThreeClassifier(ClassifierData data)
+        {
+            reTrain(data);
+        }
+
+        public IDThreeClassifier(ClassifierData data, int maxDepth)
+        {
+            this.stopEarly = false;
+            createTree(data,maxDepth);
+        }
+
+        public IDThreeClassifier(ClassifierData data, boolean stopEarly)
+        {
+            this.stopEarly = stopEarly;
+            reTrain(data);
+        }
+
+        public Classifier clone(ClassifierData data) {
+            return new IDThreeClassifier(data,this.stopEarly);
+        }
+
+        public void createTree(ClassifierData classifierData, int maxDepth) {
+            this.maxDepth = maxDepth;
+            createTree(classifierData);
+        }
+
+        public void reTrain(ClassifierData classifierData) {
+            if(!this.stopEarly)
+            {
+                createTree(classifierData);
+            }
+            else {
+                double prevError = 2;
+                double error = 1;
+                maxDepth = 1;
+                while (prevError > error)
+                {
+                    maxDepth++;
+                    prevError = error;
+                    IDThreeClassifier iDT = new IDThreeClassifier(classifierData,maxDepth);
+                    CrossValidation cv = CrossValidation.kFold(2, iDT, classifierData, 5);
+                    error = cv.mean;
+//                System.out.println("Error: " + error + " --- Depth " + maxDepth); //debug
+                }
+
+                //once best depth is known
+                createTree(classifierData,maxDepth-1);
+            }
+        }
+
+        private void createTree(ClassifierData classifierData) {
+            this.data = classifierData;
+            this.tree = new DecisionNode();
+        }
+
+        public String classify(String [] features)
+        {
+            DecisionNode node = tree;
+            while (!node.isLeaf) {
+                if (node.children.containsKey(features[node.colToSplitOn])) {
+                    node = node.children.get(features[node.colToSplitOn]);
+                } else //just take the next node
+                {
+                    node = node.children.values().iterator().next();
+                }
+
+            }
+            return node.leafClass;
+        }
+
+        private double totalEntropy(String[] a, HashSet<String> l)
+        {
+            double total = 0;
+            for (String label : l) {
+                int count = 0;
+                for (int j = 0; j < a.length; j++) {
+                    if(label.equals(a[j])){
+                        count++;
+                    }
+                }
+                total += entropy(((double) count)/a.length);
+            }
+            return total;
+        }
+
+        private class DecisionNode
+        {
+            Boolean isLeaf = false;
+            String leafClass;
+            int colToSplitOn; //the column that gives the best entropy at this node moving forward
+            String splitOnAttr; //the value the parent used to create this node
+            HashMap<String, DecisionNode> children = new HashMap<>();
+            int depth;
+            double entropy;
+            DecisionNode parent;
+            HashSet<Integer> cols = new HashSet<>();
+            HashSet<Integer> rows = new HashSet<>();
+
+            DecisionNode(){ //root
+                parent = null;
+                depth = 0;
+                for (Integer i = 0; i < data.getNumberOfDataRows(); i++) {
+                    rows.add(i);
+                }
+                for (Integer i = 0; i < data.getNumberOfDataColumns(); i++) {
+                    cols.add(i);
+                }
+                entropy = totalEntropy(data.classArray,data.listOfClasses);
+
+                double highestGain = -1;
+                int bestCol = -1;
+                for (Integer col : cols) {
+                    String[] d = data.flippedDataArray[col];
+                    HashSet<String> attrs = new HashSet<String>(Arrays.asList(d));
+                    HashMap<String,Double> classCount = new HashMap<>();
+                    double colEntropy = 0;
+                    for (String attr : attrs)
+                    {
+                        double attrCount = 0;
+                        double classEnt = 0;
+                        for (Integer row : rows) {
+                            String a = data.dataArray[row][col];
+                            if(attr.equals(a))
+                            {
+                                attrCount += data.rowWeights.get(row);
+                                String c = data.classArray[row];
+                                if(classCount.containsKey(c))
+                                {
+                                    classCount.put(c,classCount.get(c)+data.rowWeights.get(row));
+                                }
+                                else{
+                                    classCount.put(c,data.rowWeights.get(row));
+                                }
+                            }
+                        }
+                        for (String c : classCount.keySet()) {
+                            double p = (classCount.get(c))/attrCount;
+                            double e = entropy(p);
+                            classEnt += e;
+                        }
+                        classCount.clear();
+                        colEntropy += ( attrCount / getTotalRowWeight()) * classEnt;
+                    }
+                    double gain = entropy - colEntropy;
+                    if(gain>highestGain) {
+                        highestGain = gain;
+                        bestCol = col;
+                    }
+                }
+                this.colToSplitOn = bestCol;
+                HashSet<String> values = new HashSet<String>(Arrays.asList(data.flippedDataArray[bestCol]));
+                for (String attr : values) {
+                    this.children.put(attr,new DecisionNode(this, attr));
+                }
+
+            }
+
+            DecisionNode(DecisionNode parent, String splitOnAttr){
+                this.splitOnAttr = splitOnAttr;
+                this.parent = parent;
+                this.cols = new HashSet<>(parent.cols);
+                this.rows = new HashSet<>(parent.rows);
+                depth = parent.depth +1;
+//            System.out.println(depth);//debug todo
+
+                //get rows that don't match attToSplitOn
+                HashSet<Integer> toBeRemoved = new HashSet<>();;
+                for (Integer row : rows)
+                {
+                    if (!splitOnAttr.equals(data.dataArray[row][parent.colToSplitOn]))
+                        toBeRemoved.add(row);
+                }
+                rows.removeAll(toBeRemoved);
+                //remove col
+                cols.remove(parent.colToSplitOn);
+
+                //see if pure
+                HashSet<String> classesLeft = new HashSet<>();
+                String[] classRowsLeft = new String[rows.size()];
+                int i =0;
+                for(Integer row : rows)
+                {
+                    classesLeft.add(data.classArray[row]);
+                    classRowsLeft[i] = data.classArray[row];
+                    i++;
+                }
+                if ( classesLeft.size() ==1)
+                {
+                    isLeaf = true;
+                    leafClass = classesLeft.iterator().next();
+                    return;
+                }
+
+                entropy = totalEntropy(classRowsLeft,classesLeft);
+
+                if (entropy == (double)0)
+                {
+                    isLeaf = true;
+                    leafClass = data.classArray[toBeRemoved.iterator().next()];
+                    return;
+                }
+
+                if (depth >= maxDepth) //early stop
+                {
+                    double mostCommonClassCount = -1;
+                    String mostCommonClass = "";
+                    for (String c :classesLeft){
+                        double count = 0;
+                        for(int row : rows){
+                            if(c.equals(data.classArray[row]))
+                                count =+ data.rowWeights.get(row);
+                        }
+                        if (count > mostCommonClassCount)
+                        {
+                            mostCommonClass = c;
+                            mostCommonClassCount = count;
+                        }
+                    }
+                    isLeaf = true;
+                    leafClass = mostCommonClass;
+                    return;
+                }
+
+                double highestGain = -1;
+                int bestCol = -1;
+                for (Integer col : cols) {
+                    String[] d = data.flippedDataArray[col];
+                    HashSet<String> attrs = new HashSet<String>(Arrays.asList(d));
+                    HashMap<String,Double> classCount = new HashMap<>();
+                    double colEntropy = 0;
+                    for (String attr : attrs)
+                    {
+                        double attrCount = 0;
+                        double classEnt = 0;
+                        for (Integer row : rows) {
+                            String a = data.dataArray[row][col];
+                            if(attr.equals(a))
+                            {
+                                attrCount+= data.rowWeights.get(row);
+                                String c = data.classArray[row];
+                                if(classCount.containsKey(c))
+                                {
+                                    classCount.put(c,classCount.get(c)+data.rowWeights.get(row));
+                                }
+                                else{
+                                    classCount.put(c,data.rowWeights.get(row));
+                                }
+                            }
+                        }
+                        for (String c : classCount.keySet()) {
+                            double p = classCount.get(c)/attrCount;
+                            double e = entropy(p);
+                            classEnt += e;
+                        }
+                        classCount.clear();
+                        colEntropy += attrCount / getTotalRowWeight() * classEnt;
+                    }
+                    double gain = entropy - colEntropy;
+                    if(gain>highestGain) {
+                        highestGain = gain;
+                        bestCol = col;
+                    }
+                }
+                this.colToSplitOn = bestCol;
+                HashSet<String> values = new HashSet<String>(Arrays.asList(data.flippedDataArray[bestCol]));
+                for (String attr : values) {
+                    this.children.put(attr,new DecisionNode(this, attr));
+                }
+            }
+
+        }
+
+        private double getTotalRowWeight()
+        {
+            double total = 0;
+            for (int i = 0; i < data.getNumberOfDataRows(); i++) {
+                total += data.rowWeights.get(i);
+            }
+            return total;
+        }
+
+        private double entropy(double p) {
+            if (p == 0 || p == 1)
+                return 0;
+            return -p * (Math.log(p)/Math.log(2));
+        }
+    }
+
 
 }
