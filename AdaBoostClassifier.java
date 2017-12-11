8bb3feaf92a827fc429458befe76f73bf32aca35
TheFamousDirector <FamousDirector@users.noreply.github.com>
2017-12-11 15:13:03 -0400

    added one more check for no attributes left

--- AdaBoostClassifier.java
+++ AdaBoostClassifier.java
@@ -453,6 +453,28 @@ public class AdaBoostClassifier implements Classifier {
                 }
                 entropy = totalEntropy(data.classArray,data.listOfClasses,data.rowWeights);
 
+                if (cols.size() == 0) //no splitting left
+                {
+                    isLeaf = true;
+
+                    int highestCount = -1;
+                    String highestClass = "";
+                    for (String c : data.listOfClasses) {
+                        int count = 0;
+                        for (String r : data.classArray) {
+                            if (r.equals(c))
+                                count++;
+                        }
+                        if(highestCount < count)
+                        {
+                            highestClass = c;
+                            highestCount = count;
+                        }
+                    }
+                    leafClass = highestClass;
+                    return;
+                }
+
                 double highestGain = -1;
                 int bestCol = -1;
                 for (Integer col : cols) {
